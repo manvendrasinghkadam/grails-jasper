e995f40729c4b9039d1a58746f2ae081e58ba8c1
Manvendra SK <manvendrasingh.1990@gmail.com>
2015-07-29 14:19:09 +0530

    Updated travis configuration.

--- travis-build.sh
+++ travis-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-./grailsw refresh-dependencies --non-interactive
-./grailsw test-app --non-interactive
-./grailsw package-plugin --non-interactive
-./grailsw doc --pdf --non-interactive
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == 'candrews/grails-jasper' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+#./grailsw refresh-dependencies --non-interactive
+#./grailsw test-app --non-interactive
+#./grailsw package-plugin --non-interactive
+#./grailsw doc --pdf --non-interactive
+#if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == 'candrews/grails-jasper' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   git config --global user.name "$GIT_NAME"
   git config --global user.email "$GIT_EMAIL"
   git config --global credential.helper "store --file=~/.git-credentials"
@@ -12,16 +12,16 @@ if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == 'candrews/grails-jasper
   git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
   cd gh-pages
   git rm -rf .
-  cp -r ../target/docs/. ./
+  cp -r ../build/docs/. ./
   git add *
   git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
   git push origin HEAD
   cd ..
   rm -rf gh-pages
-  ./grailsw publish-plugin --no-scm --allow-overwrite --non-interactive
-else
-  echo "Not on master branch, so not publishing"
-  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
-  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
-fi
+#  ./grailsw publish-plugin --no-scm --allow-overwrite --non-interactive
+#else
+#  echo "Not on master branch, so not publishing"
+#  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+#  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+#  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+#fi
