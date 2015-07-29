b625d5906fd9b78bba3d7b347f95a41748a74e61
Manvendra SK <manvendrasingh.1990@gmail.com>
2015-07-29 15:26:55 +0530

    Reverted to Andrew's credentials.

--- travis-build.sh
+++ travis-build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
-#./grailsw refresh-dependencies --non-interactive
-#./grailsw test-app --non-interactive
-#./grailsw package-plugin --non-interactive
-#./grailsw doc --pdf --non-interactive
-
 ./gradlew clean build groovydoc assemble
 
-#if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == 'candrews/grails-jasper' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == 'candrews/grails-jasper' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   git config --global user.name "$GIT_NAME"
   git config --global user.email "$GIT_EMAIL"
   git config --global credential.helper "store --file=~/.git-credentials"
@@ -15,16 +10,16 @@ set -e
   git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
   cd gh-pages
   git rm -rf .
-  cp -vr ../build/docs/. ./
+  cp -r ../build/docs/. ./
   git add *
   git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
   git push origin HEAD
   cd ..
   rm -rf gh-pages
 #  ./grailsw publish-plugin --no-scm --allow-overwrite --non-interactive
-#else
-#  echo "Not on master branch, so not publishing"
-#  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-#  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
-#  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
-#fi
+else
+  echo "Not on master branch, so not publishing"
+  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+fi
